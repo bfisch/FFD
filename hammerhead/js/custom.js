7747873739920663c811733422f778d33d4d5d78
Brian Fischer <bfischer@astonish.com>
2015-11-11 15:04:20 -0500

    Slider Arrows

--- hammerhead/js/custom.js
+++ hammerhead/js/custom.js
@@ -1,5 +1,19 @@
 jQuery(window).scroll(function () {
 
+    var scrollHeight = jQuery(document).scrollTop();
+    var shadowDepth = scrollHeight;
+    var killHeight = 400;
+    if (scrollHeight > killHeight) {var shadowDepth = killHeight;}
+  
+    if (scrollHeight > 0) {
+      jQuery('#navigationContainer').css('box-shadow','5px 0px 15px rgba(31,38,47,'+shadowDepth/killHeight+')');
+    } else if (scrollHeight > killHeight) {
+      jQuery('#navigationContainer').css('box-shadow','3px 0px 10px rgba(31,38,47,1)');
+    } else {
+      jQuery('#navigationContainer').css('box-shadow','3px 0px 10px rgba(31,38,47,0)');
+    }
+  
+    
 });
 
 	var headerHeight = jQuery('#navigationContainer').outerHeight();
